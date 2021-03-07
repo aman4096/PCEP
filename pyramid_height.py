d989f737de372fcafd2a128053b27abf8a49ac3b
Aman Kumar <aman4096@gmail.com>
2021-03-07 10:58:31 +0530

    pyramid-height

---
+++ pyramid_height.py
@@ -0,0 +1,14 @@
+blocks = int(input("Enter the number of blocks: "))
+
+#
+# Write your code here.
+#
+layer_length=0
+blocks_used=0
+while(blocks>layer_length):
+    layer_length+=1
+    blocks_used+=layer_length
+    blocks-=layer_length
+    
+
+print("The height of the pyramid:", layer_length)
